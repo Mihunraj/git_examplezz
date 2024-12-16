76710063409f1734be1debc5cbdaa82e769514f8
Your Name <you@example.com>
2024-12-16 20:09:57 +0530

    this is example of centralized workflow

---
+++ Fibonacci_Series.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = scanner.nextInt();
+        int a = 0, b = 1;
+
+        System.out.print("Fibonacci Series: " + a + ", " + b);
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(", " + next);
+            a = b;
+            b = next;
+        }
+    }
+}
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+Fibonacci_Series.java [unix] (05:29 01/01/1970)                                                                                                                                                          0,0-1 All
+"Fibonacci_Series.java" [New]
+
